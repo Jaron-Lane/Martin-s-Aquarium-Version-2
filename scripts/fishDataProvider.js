4effe2c0539f512ca453de36b480fbd8e2568329
Jaron-Lane <jaronwohlwend@icloud.com>
2020-10-08 09:55:50 -0500

    Second version of Martins Aquarium

---
+++ scripts/fishDataProvider.js
@@ -0,0 +1,55 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+
+
+const fishCollection = [
+    {
+        name: "Kyle",
+        species: "Butterfly Fish",
+        diet: "Algea, Corals, and Sponges",
+        length: "20cm",
+        harvestLocation: "Indian Ocean",
+        image: "https://www.thesprucepets.com/thmb/EcwXL_b2vz1zDqGsvlK6CPEPBRE=/1806x1200/filters:fill(auto,1)/butterflyfish-588a0a803df78caebcc08582.jpg"
+
+    }
+    {
+        name: "Kyle 2",
+        species: "Butterfly Fish",
+        diet: "Algea, Corals, and Sponges",
+        length: "20cm",
+        harvestLocation: "Indian Ocean",
+        image: "https://www.thesprucepets.com/thmb/EcwXL_b2vz1zDqGsvlK6CPEPBRE=/1806x1200/filters:fill(auto,1)/butterflyfish-588a0a803df78caebcc08582.jpg"
+
+    }
+    {
+        name: "Kyle the III",
+        species: "Butterfly Fish",
+        diet: "Algea, Corals, and Sponges",
+        length: "20cm",
+        harvestLocation: "Indian Ocean",
+        image: "https://www.thesprucepets.com/thmb/EcwXL_b2vz1zDqGsvlK6CPEPBRE=/1806x1200/filters:fill(auto,1)/butterflyfish-588a0a803df78caebcc08582.jpg"
+
+    }
+    {
+        name: "Kyle Cuatro",
+        species: "Butterfly Fish",
+        diet: "Algea, Corals, and Sponges",
+        length: "20cm",
+        harvestLocation: "Indian Ocean",
+        image: "https://www.thesprucepets.com/thmb/EcwXL_b2vz1zDqGsvlK6CPEPBRE=/1806x1200/filters:fill(auto,1)/butterflyfish-588a0a803df78caebcc08582.jpg"
+
+    }
+    {
+        name: "Kyle.5",
+        species: "Butterfly Fish",
+        diet: "Algea, Corals, and Sponges",
+        length: "20cm",
+        harvestLocation: "Indian Ocean",
+        image: "https://www.thesprucepets.com/thmb/EcwXL_b2vz1zDqGsvlK6CPEPBRE=/1806x1200/filters:fill(auto,1)/butterflyfish-588a0a803df78caebcc08582.jpg"
+
+    }
+]
